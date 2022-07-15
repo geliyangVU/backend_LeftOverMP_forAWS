0565165d6bd9ecaab456d8d104aa5a25ef6c76f1
Geli Yang <gyang5@villanova.edu>
2022-07-15 18:05:57 -0400

    format routes.js file

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { getAllUsers,getUser,editPassword,deleteUser,loginUser,registerUser,followUser,unfollowUser}=require('../controllers/userController')
-const{getAllItems,uploadItem,deleteItem, getItem}=require('../controllers/itemController')
+const { getAllUsers, getUser, editPassword, deleteUser, loginUser, registerUser, followUser, unfollowUser } = require('../controllers/userController')
+const { getAllItems, uploadItem, deleteItem, getItem } = require('../controllers/itemController')
 
 //User Controller
 router.get('/users/getAllUsers', getAllUsers)
-router.post('/users/register',registerUser)
-router.post('/users/login',loginUser)
+router.post('/users/register', registerUser)
+router.post('/users/login', loginUser)
 
 
 
@@ -15,18 +15,18 @@ router.get('/users/getUser/:id', getUser)
 
 
 //allow User to edit password.  (passing id as parameter)
-router.put('/users/editPassword/:id',editPassword)
-router.delete('/users/deleteUser',deleteUser)
-router.put('/users/:id/follow',followUser)
-router.put('/users/:id/unfollow',unfollowUser)
+router.put('/users/editPassword/:id', editPassword)
+router.delete('/users/deleteUser', deleteUser)
+router.put('/users/:id/follow', followUser)
+router.put('/users/:id/unfollow', unfollowUser)
 
 
 
 
 // Item Controller
-router.get('/items/getAllItems',getAllItems)
-router.post('/items/uploadItem',uploadItem)
-router.delete('/items/deleteItem',deleteItem)
-router.get('/items/getItem',getItem)
+router.get('/items/getAllItems', getAllItems)
+router.post('/items/uploadItem', uploadItem)
+router.delete('/items/deleteItem', deleteItem)
+router.get('/items/getItem', getItem)
 
-module.exports=router
+module.exports = router
