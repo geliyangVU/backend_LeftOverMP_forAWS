e600d8bcc04f95a8558ab4a70d3fb57504b455e6
Geli Yang <gyang5@villanova.edu>
2022-07-18 21:59:03 -0400

    initial Implementation of uploadPost

---
+++ controllers/PostController.js
@@ -0,0 +1,24 @@
+
+
+const uploadPost = asyncHandler(async (req, res) => {
+    const { price, expirationDate, nameOfItem, description, ingredients_allergens } = req.body
+
+    
+    const post = await Post.create({
+        price, expirationDate, nameOfItem, description, ingredients_allergens
+    })
+
+    if (post) {
+        res.status(201).json({
+            price: post.price,
+            expirationDate: post.expirationDate,
+            name: post.name,
+            description: post.description,
+            message: "Post uploaded"
+        })
+
+    } else {
+        res.status(400)
+        throw new Error('Invalid entry')
+    }
+})
