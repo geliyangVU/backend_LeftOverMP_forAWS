716cf26707ffb97b8ebef1a3d1adffd89c512980
Geli Yang <gyang5@villanova.edu>
2022-07-20 20:28:12 -0400

    test

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,23 @@
-// const express = require("express");
-// const res = require("express/lib/response");
-// const router = express.Router();
+const express = require("express");
+const res = require("express/lib/response");
+const router = express.Router();
 
-// router.get("/", async (req, res) => {
-//   return res.status(200).json({
-//     title: "heroku for LeftOverMarketPlace",
-//     message: "working",
-//     message2: "this is the home route"
-//   });
-// });
+router.get("/", async (req, res) => {
+  const returnHTML=`<h1>LeftOverBackend</h1>
+  <div>This is homepage</div>`
 
 
-// // router.get("/1", async (req, res) => {
-// //   const returnhtml=`<form action="/upload" method="post" enctype="multipart/form-data">
-// //   <label for="file">File:</label>
-// //   <input type="file" id="file" name="file" required />
-// //   <button style="grid-column: span 2;" type="submit">Share</button>
-// // </form>`
-// //   res.send(returnhtml)
-// //  });
+  return res.status(200).send(returnHTML)
+});
 
-// module.exports = router;
+
+// router.get("/1", async (req, res) => {
+//   const returnhtml=`<form action="/upload" method="post" enctype="multipart/form-data">
+//   <label for="file">File:</label>
+//   <input type="file" id="file" name="file" required />
+//   <button style="grid-column: span 2;" type="submit">Share</button>
+// </form>`
+//   res.send(returnhtml)
+//  });
+
+module.exports = router;
